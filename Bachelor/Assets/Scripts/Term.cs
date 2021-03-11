d9303aa63ff082f9405a4d76944e774960c38db1
MatQuaade <Tquaade@gmail.com>
2021-03-11 13:25:46 +0100

    Update the terms prefab to be clickable text, so we can use that for multiple choice. Made Prefab variant for terms. each term has component Term, which adds functionality for displaying description

---
+++ Bachelor/Assets/Scripts/Term.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Term : MonoBehaviour, IDeselectHandler
+{
+    [SerializeField]
+    Text descriptionText;
+    [SerializeField]
+    string description = "";
+    // Start is called before the first frame update
+    void Start()
+    {
+        descriptionText.text = description;
+        descriptionText.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void DisplayDescription()
+    {
+        Debug.Log("Display description for term");
+        descriptionText.enabled = true;
+    }
+
+    public void OnDeselect(BaseEventData data)
+    {
+        Debug.Log("Deselected");
+        descriptionText.enabled = false;
+    }
+}
