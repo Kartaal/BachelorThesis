b75737bb10026e375bf0946db63ee31794066148
MatQuaade <Tquaade@gmail.com>
2021-03-11 12:36:52 +0100

    Added term button as prefab, and made a prefab for the collected list of terms. Added a script for controlling and displaying decriptions when a term is click. No functionality yest

--- Bachelor/Assets/Scripts/SceneController.cs
+++ Bachelor/Assets/Scripts/SceneController.cs
@@ -20,6 +20,7 @@ public class SceneController : MonoBehaviour {
         }
         */
     }
+    
 
     /*
     IEnumerator WaitForTime()
---
+++ Bachelor/Assets/Scripts/TermsController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TermsController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DisplayDescription()
+    {
+        Debug.Log("Display description for term");
+    }
+}
