9544fdc104c2e9e3c8c5c98689c2eaa2e213f809
niva <niva@itu.dk>
2021-03-11 12:07:41 +0100

    Made prefab navigation element with arrows. Learn YDS branch, credits and resources have navigation element. Also removed some prefab mess I had made.

--- Bachelor/Assets/Scripts/SceneController.cs
+++ Bachelor/Assets/Scripts/SceneController.cs
@@ -7,18 +7,65 @@ public class SceneController : MonoBehaviour {
 
     /*
     [SerializeField] int timeToWait = 4;
-    int currentSceneIndex;
+    
     */
+    int currentSceneIndex;
+
+    /*
+     * This refers to the scene to change to, going backwards in a branch will
+     * still update this variable !!
+     * This variable is updated by CalculateNextSceneIndex() and CalculatePreviousSceneIndex()
+     */
+    private int nextSceneIndex = 19; // temp debug setting default behaviour for nav arrows to go to credits scene
 
     // Use this for initialization
     void Start () {
-        /*
         currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        /*
         if (currentSceneIndex == 0)
         {
             StartCoroutine(WaitForTime());
         }
         */
+    }
+
+    private void CalculateNextSceneIndex()
+    {
+        switch (currentSceneIndex)
+        {
+            case 5:
+            case 10:
+            case 13:
+            case 16:
+            case 18:
+            case 19:
+            case 20:
+                        nextSceneIndex = 0; // if end of branch, go home (??)
+                        break;
+            default:
+                        nextSceneIndex = currentSceneIndex + 1;
+                        break;
+        }
+    }
+
+    private void CalculatePreviousSceneIndex()
+    {
+        switch (currentSceneIndex)
+        {
+            case 1:
+            case 6:
+            case 11:
+            case 14:
+            case 17:
+            case 19:
+            case 20:
+                        nextSceneIndex = 0; // if start of branch, go home
+                        break;
+            default:
+                        nextSceneIndex = currentSceneIndex - 1;
+                        break;
+        }
     }
 
     /*
@@ -45,6 +92,18 @@ public class SceneController : MonoBehaviour {
         SceneManager.LoadScene(currentSceneIndex + 1);
     }
     */
+
+    public void LoadNextScene()
+    {
+        CalculateNextSceneIndex();
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    public void LoadPreviousScene()
+    {
+        CalculatePreviousSceneIndex();
+        SceneManager.LoadScene(nextSceneIndex);
+    }
 
     public void LoadHome()
     {
