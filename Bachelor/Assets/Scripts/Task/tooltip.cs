f76d28e2c06e5c3b2c8c283bb5bafe184f90fc47
Philip Bernth Johansen <phij@itu.dk>
2021-03-17 12:48:10 +0100

    tooltips now display information of their associated tasks, including workload, release, deadline and ID-X number

--- Bachelor/Assets/Scripts/Task/tooltip.cs
+++ Bachelor/Assets/Scripts/Task/tooltip.cs
@@ -6,12 +6,25 @@ using UnityEngine.EventSystems;
 
 public class tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    [SerializeField]
     private GameObject tt; //shorthand for tooltip
+    [SerializeField]
+    private Task taskData; // reference to Task.cs script attatched to this object for data.
+    [SerializeField]
+    private Text wrkTXT, relTXT, dedTXT;
+    private Text IDTXT;
+
 
     void Start()
     {
         tt = gameObject.transform.Find("TextBlock").gameObject;
-        Debug.Log("Start");
+        taskData = gameObject.GetComponent<Task>();
+        
+        wrkTXT = tt.transform.Find("WrkValue").GetComponent<Text>();
+        relTXT = tt.transform.Find("RelValue").GetComponent<Text>();
+        dedTXT = tt.transform.Find("DedValue").GetComponent<Text>();
+        IDTXT = tt.transform.Find("idText").GetComponent<Text>();
+        initializeToolTipInformation();
     }
 
     // Update is called once per frame
@@ -20,6 +33,24 @@ public class tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
         
     }
 
+    //Called once to update ID and all fields to initial task values.
+    private void initializeToolTipInformation(){
+        IDTXT.text = "ID-" + taskData.GetId();
+        UpdateToolTipInformation();
+    }
+
+    //can be called from anywhere to update all fields of text.
+    public void UpdateToolTipInformation(){
+        wrkTXT.text = "" + taskData.GetWork();
+        relTXT.text = "" + taskData.GetRelease();
+        dedTXT.text = "" + taskData.GetDeadline();
+
+
+    }
+
+/*
+    Using unity's event system to record mouse interactions for tooltips popup
+*/
     public void OnPointerEnter(PointerEventData ped){
         Debug.Log("Mouse Enter");
         tt.SetActive(true);
@@ -29,20 +60,5 @@ public class tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
         Debug.Log("Mouse Exit");
         tt.SetActive(false);
     }
-     /*
- 
-     public void OnPointerEnter(PointerEventData eventData)
-     {
-         mouse_over = true;
-         Debug.Log("Mouse enter");
-     }
- 
-     public void OnPointerExit(PointerEventData eventData)
-     {
-         mouse_over = false;
-         Debug.Log("Mouse exit");
-     }
-     
-     */
 
 }
