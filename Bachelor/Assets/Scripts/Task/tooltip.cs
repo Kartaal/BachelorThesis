9a3a712355b3f714bb2a08cc9e756a944a596c07
Philip Bernth Johansen <phij@itu.dk>
2021-03-17 12:03:08 +0100

    made basic tooltip implementation on develop scene. has show and hide fucntionality.

---
+++ Bachelor/Assets/Scripts/Task/tooltip.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private GameObject tt; //shorthand for tooltip
+
+    void Start()
+    {
+        tt = gameObject.transform.Find("TextBlock").gameObject;
+        Debug.Log("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData ped){
+        Debug.Log("Mouse Enter");
+        tt.SetActive(true);
+    }
+    
+    public void OnPointerExit(PointerEventData ped){
+        Debug.Log("Mouse Exit");
+        tt.SetActive(false);
+    }
+     /*
+ 
+     public void OnPointerEnter(PointerEventData eventData)
+     {
+         mouse_over = true;
+         Debug.Log("Mouse enter");
+     }
+ 
+     public void OnPointerExit(PointerEventData eventData)
+     {
+         mouse_over = false;
+         Debug.Log("Mouse exit");
+     }
+     
+     */
+
+}
