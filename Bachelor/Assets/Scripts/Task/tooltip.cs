97ca04eb050223ccb889c934ce81fec2e628520d
Philip Bernth Johansen <phij@itu.dk>
2021-03-17 12:50:44 +0100

    cleaned up code a little bit. Still have some layering issues.

--- Bachelor/Assets/Scripts/Task/tooltip.cs
+++ Bachelor/Assets/Scripts/Task/tooltip.cs
@@ -6,13 +6,12 @@ using UnityEngine.EventSystems;
 
 public class tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    [SerializeField]
+    
     private GameObject tt; //shorthand for tooltip
-    [SerializeField]
+    
     private Task taskData; // reference to Task.cs script attatched to this object for data.
-    [SerializeField]
-    private Text wrkTXT, relTXT, dedTXT;
-    private Text IDTXT;
+    
+    private Text wrkTXT, relTXT, dedTXT, IDTXT;
 
 
     void Start()
@@ -20,17 +19,12 @@ public class tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
         tt = gameObject.transform.Find("TextBlock").gameObject;
         taskData = gameObject.GetComponent<Task>();
         
+        //initializes Text Fields so they can be edited.
         wrkTXT = tt.transform.Find("WrkValue").GetComponent<Text>();
         relTXT = tt.transform.Find("RelValue").GetComponent<Text>();
         dedTXT = tt.transform.Find("DedValue").GetComponent<Text>();
         IDTXT = tt.transform.Find("idText").GetComponent<Text>();
         initializeToolTipInformation();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     //Called once to update ID and all fields to initial task values.
@@ -44,20 +38,16 @@ public class tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
         wrkTXT.text = "" + taskData.GetWork();
         relTXT.text = "" + taskData.GetRelease();
         dedTXT.text = "" + taskData.GetDeadline();
-
-
     }
 
 /*
     Using unity's event system to record mouse interactions for tooltips popup
 */
     public void OnPointerEnter(PointerEventData ped){
-        Debug.Log("Mouse Enter");
         tt.SetActive(true);
     }
     
     public void OnPointerExit(PointerEventData ped){
-        Debug.Log("Mouse Exit");
         tt.SetActive(false);
     }
 
