52c02f837f503438e8b911e7007024919da035ff
MatQuaade <matq@itu.dk>
2021-03-18 10:19:40 +0100

    Added functionality, to the multiple choice. Question text, and answer option text is displayed. and it's possible to select a answer, and it will be displayed whether is it is correct or not. When selecting next question the answers will be reset

---
+++ Bachelor/Assets/Scripts/MultipleChoiceManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using System;
+
+public class MultipleChoiceManager : MonoBehaviour
+{
+    [SerializeField]
+    private Question[] questions = new Question[5];
+
+    [SerializeField]
+    private Toggle[] answerOptions;
+
+    [SerializeField]
+    private ToggleGroup answers;
+
+    private Question currentQuestion;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        answerOptions = answers.GetComponentsInChildren<Toggle>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void DisplayAnswers()
+    {
+        foreach (Question q in questions)
+        {
+            Toggle theActiveToggle = answers.ActiveToggles().FirstOrDefault();
+            if(theActiveToggle != null) { theActiveToggle.isOn = false; }
+            
+            foreach (Toggle t in answerOptions)
+            {
+                t.GetComponentInChildren<Image>().color = new Color32(255, 255, 255, 255);
+            }
+            if (q.IsActive())
+            {
+                currentQuestion = q;
+                var temp = q.GetAnswerOptions();
+                for (int i = 0; i < temp.Length; i++)
+                {
+                    answerOptions[i].GetComponentInChildren<Text>().text = temp[i];
+                }
+            }
+        }
+    }
+
+    public void CheckAnswer()
+    {
+        Toggle theActiveToggle = answers.ActiveToggles().FirstOrDefault();
+        if (theActiveToggle != null)
+        {
+            string answer = theActiveToggle.GetComponentInChildren<Text>().text;
+
+            foreach (Question q in questions)
+            {
+                if (q == currentQuestion)
+                {
+                    if (Array.IndexOf(q.GetAnswerOptions(), answer) == q.GetAnswer())
+                    {
+                        theActiveToggle.GetComponentInChildren<Image>().color = new Color32(92, 197, 101, 255);
+                    }
+                    else
+                    {
+                        theActiveToggle.GetComponentInChildren<Image>().color = new Color32(198, 92, 92, 255);
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ Bachelor/Assets/Scripts/Question.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Question : MonoBehaviour, IDeselectHandler
+{
+    [SerializeField]
+    string[] answerOptions = new string[4];
+
+    [SerializeField]
+    private string question = "";
+
+    [SerializeField]
+    private int answer = -1;
+
+    [SerializeField]
+    Text descriptionText;
+
+    private bool isActive = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DisplayDescription()
+    {
+        descriptionText.text = question;
+        Debug.Log("Display description for term");
+        descriptionText.enabled = true;
+        isActive = true;
+    }
+
+    public void OnDeselect(BaseEventData data)
+    {
+        Debug.Log("Deselected");
+        isActive = false;
+    }
+
+    public string GetQuestion() { return question; }
+
+    public int GetAnswer() { return answer; }
+
+    public string[] GetAnswerOptions() { return answerOptions; }
+
+    public bool IsActive() { return isActive; }
+}
