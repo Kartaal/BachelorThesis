d6fdacb0568764402d4983712a5d1f2582f8b5db
Niclas Valentiner <niva@itu.dk>
2021-04-07 09:59:32 +0200

    Updated terms text, made description multiline to display correctly.

--- Bachelor/Assets/Scripts/Term.cs
+++ Bachelor/Assets/Scripts/Term.cs
@@ -9,6 +9,7 @@ public class Term : MonoBehaviour
     [SerializeField]
     Text descriptionText;
     [SerializeField]
+    [Multiline]
     string description = "";
     // Start is called before the first frame update
     void Start()
